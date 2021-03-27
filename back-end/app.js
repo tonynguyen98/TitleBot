f25c6ae3c9c2a4ae3ce3c05202fbee86b49aa71a
Tony <tonynguyen0925@gmail.com>
2021-03-27 14:50:16 -0700

    Created a simple scraper that will get <title>

---
+++ back-end/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
+const port = 3000;
+
+const parseTitle = (body) => {
+  // <title> regex courtesy of https://stackoverflow.com/a/13452897/12454586
+  let title = body.match(/<title[^>]*>([^<]+)<\/title>/);
+
+  if (!title) {
+    throw new Error("Unable to get the title of the webpage");
+  }
+
+  return title[1];
+};
+
+app.get("/", (req, res) => {
+  const { url } = req.query;
+  if (!url) return res.status(400).end("Empty url query");
+
+  // node-fetch package
+  fetch(url)
+    // parse the response into text
+    .then((res) => res.text())
+    // use parseTitle function to get title
+    .then((resText) => parseTitle(resText))
+    // return the title to the request
+    .then((title) => res.send(title))
+    // catch any errors
+    .catch((e) => res.status(500).end(e.message));
+});
+
+app.listen(port, () => {
+  console.log(`TitleBot scraper listening at http://localhost:${port}`);
+});
